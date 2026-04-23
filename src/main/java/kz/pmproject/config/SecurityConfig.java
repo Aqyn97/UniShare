30875705f4de47d0f65606c03f87269534859739
yeraassyltl <eratulegen99@gmail.com>
2026-04-23 19:00:02 +0000

    fix: remove unused Customizer import

--- src/main/java/kz/pmproject/config/SecurityConfig.java
+++ src/main/java/kz/pmproject/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package kz.pmproject.config;
 import kz.pmproject.config.filter.TokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
