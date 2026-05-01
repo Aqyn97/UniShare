91bfac966a95afbf4c2b005a34a8059bdc468701
nurdaulettn <nurdaulettolegen26@gmail.com>
2026-05-01 21:21:41 +0500

    add cors for frontend

---
+++ src/main/java/kz/pmproject/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package kz.pmproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://frontend-production-9056.up.railway.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
